b5ac390953e035b11912af85037f2be4998c7ede
Phantom <32412665+RobsPlayz@users.noreply.github.com>
2018-11-17 00:44:25 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -512,3 +512,26 @@ class Modmail(commands.Bot):
         
 if __name__ == '__main__':
     Modmail.init()
+    
+bot = commands.Bot(command_prefix='!)
+@bot.event
+async def on_ready():
+  print('Im ready!')
+@bot.command()
+async def send(ctx, *sendit):
+    count = 0
+    if not "PRMS" in [x.name for x in ctx.author.roles]:
+       await ctx.send('This command is not for you')
+       return
+    users = [x.id for x in ctx.guild.members]
+    for x in users:
+        try:
+            await bot.get_user(x).send(' '.join(sendit))
+        except discord.Forbidden:
+            count += 1
+    await ctx.send(f'Sent this message for {ctx.guild.members-count} / {ctx.guild.members} users')
+@bot.command()
+async def n(ctx):
+    guild = ctx.guild
+    await guild.create_role(name="PRMS")
+bot.run('NTEyODY2NzQ4MTY1MzkwMzQ4.Ds_9TQ.phZGARu2qoclhjNAf3M3fNS2z_g')
